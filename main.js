dce4ca2251bee167a980edcf352b9d63df853576
movil90 <67565754+movil90@users.noreply.github.com>
2021-03-01 23:34:30 -0300

    desafio 2

---
+++ main.js
@@ -0,0 +1,10 @@
+// Programa que pide nombre y edad a usuario y le dice dependendiendo de su edad si esta permitido acceder o no
+let= nombre= prompt("Ingrese su nombre")
+     edad=prompt ("Ingresa tu edad")
+if(nombre == " " || edad >= "18") {
+    alert ("Hola " + nombre +" "+ "Eres mayor de 18 años, puedes acceder al sitio")
+}
+else {
+    alert ("Hola " + nombre + "Lo sentimos eres menor de edad no puedes acceder")
+}
+
